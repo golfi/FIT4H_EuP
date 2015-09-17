8511687388da9557ed7cb85b57d60b1c06c05e79
golfi <bennizaschke@web.de>
2015-09-17 14:17:44 +0200

    Ruby Start

--- ruby/code/flicks.rb
+++ ruby/code/flicks.rb
@@ -1,33 +1,72 @@
 class Movie
-	
 	def initialize(ptitle, prank=0)
 		@title = ptitle.capitalize
 		@rank = prank
 	end
+	# def title
+	# 	@title
+	# end
+	attr_reader :title
 
+	# def title=(ptitle)
+	# 	@title = ptitle
+	# end
+	attr_writer :title
+	
 	def thumbs_up
 		@rank += 1
 		#@rank = @rank + 1
 	end
-
 	def thumbs_down
 		@rank -= 1
-		#@rank = @rank - 1
 	end
-
 	def to_s
 		"#{@title} hat ein Ranking von: #{@rank}."
 	end
-
 end
 
-obj_movie1 = Movie.new('SWAT', 4)
-puts obj_movie1.to_s
-obj_movie1.thumbs_up
-puts obj_movie1.to_s
-obj_movie1.thumbs_down
-obj_movie1.thumbs_down
-puts obj_movie1.to_s
+movie1 = Movie.new('NinjaTurtles',4)
+puts movie1.to_s
+movie1.thumbs_up
+puts movie1.to_s
+
+puts movie1.title
+
+movie1.title = "ninjaPurples"
+puts movie1
+
+
+
+# class Movie
+	
+# 	def initialize(ptitle, prank=0)
+# 		@title = ptitle.capitalize
+# 		@rank = prank
+# 	end
+
+# 	def thumbs_up
+# 		@rank += 1
+# 		#@rank = @rank + 1
+# 	end
+
+# 	def thumbs_down
+# 		@rank -= 1
+# 		#@rank = @rank - 1
+# 	end
+
+# 	def to_s
+# 		"#{@title} hat ein Ranking von: #{@rank}."
+# 	end
+
+# end
+
+# obj_movie1 = Movie.new('SWAT', 4)
+# puts obj_movie1.to_s
+# obj_movie1.thumbs_up
+# puts obj_movie1.to_s
+# obj_movie1.thumbs_down
+# obj_movie1.thumbs_down
+# puts obj_movie1.to_s
 
 
 # def movie_listing(title, rank=10)
