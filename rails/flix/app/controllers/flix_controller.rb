ecbf329043b7ed16402ca39770252c12962f152f
golfi <bennizaschke@web.de>
2015-11-26 15:04:05 +0100

    Ruby Start

--- rails/flix/app/controllers/flix_controller.rb
+++ rails/flix/app/controllers/flix_controller.rb
@@ -7,4 +7,35 @@ class FlixController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 	end
+
+	def edit
+		@movie  = Movie.find(params[:id])		
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		@movie.update(movie_params)
+		redirect_to flix_path(@movie.id)
+	end
+
+	def new
+		@movie =  Movie.new
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+		@movie.save
+		redirect_to flix_path(@movie.id)		
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		redirect_to flix_index_path		
+	end
+
+	private
+		def movie_params
+			permitted_params = params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
+		end
 end
