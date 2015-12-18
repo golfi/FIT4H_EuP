1cdfc38d4e6e1aef1a9cc2ac7f403df7bf21610e
golfi <bennizaschke@web.de>
2015-12-18 09:37:44 +0100

    Ruby Start

--- rails/rails_app/app/controllers/registrations_controller.rb
+++ rails/rails_app/app/controllers/registrations_controller.rb
@@ -1,7 +1,32 @@
 class RegistrationsController < ApplicationController
+
+	before_action :set_event
 	
 	def index
-		@event = Event.find(params[:event_id])
 		@registrations = @event.registrations
 	end
+
+	def new
+		@registration = @event.registrations.new
+	end
+
+	def create
+		@registration = @event.registrations.new(registrations_params)
+		if @registration.save
+			redirect_to event_path(@event.id), notice: "Registrierung erfolgreich!"
+		else
+			flash[:alert] = "woher kennst das?!"
+			render "new"
+		end
+	end
+
+	private
+
+		def registrations_params
+			params.require(:registration).permit(:name, :email, :how_heard)			
+		end
+
+		def set_event
+			@event = Event.find(params[:event_id])	
+		end
 end
