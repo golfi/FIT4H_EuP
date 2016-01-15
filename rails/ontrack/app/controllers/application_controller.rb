0a5d0caf56be95adfa8ad679ae6170d2b3af8532
golfi <bennizaschke@web.de>
2016-01-15 14:32:32 +0100

    Ruby Start

--- rails/ontrack/app/controllers/application_controller.rb
+++ rails/ontrack/app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
   	@current_user = User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def require_signin
+  	unless current_user
+  		redirect_to signin_path, alert: "Erst einloggen!"
+  	end
+  end
+  
 end
